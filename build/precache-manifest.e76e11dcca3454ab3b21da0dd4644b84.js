aa7ade8c3d6abedf0830d8b5d8f6fbfa5cc471df
Arturo Linares <arturo.linares@gmail.com>
2020-08-19 11:59:49 -0500

    Added the build.

---
+++ build/precache-manifest.e76e11dcca3454ab3b21da0dd4644b84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779af9fec1aa5553863cec5e39ffae30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d1c9a22c64db4aea52c",
+    "url": "/static/css/2.16816ef8.chunk.css"
+  },
+  {
+    "revision": "74a4ca1dedba1b7e7b3c",
+    "url": "/static/css/main.f01e94cb.chunk.css"
+  },
+  {
+    "revision": "6d1c9a22c64db4aea52c",
+    "url": "/static/js/2.e229a733.chunk.js"
+  },
+  {
+    "revision": "555e3d9985a574b9555a465f4c721686",
+    "url": "/static/js/2.e229a733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a4ca1dedba1b7e7b3c",
+    "url": "/static/js/main.c2ef8d15.chunk.js"
+  },
+  {
+    "revision": "81210570a5d0771c239a",
+    "url": "/static/js/runtime-main.d291746f.js"
+  }
+]);
