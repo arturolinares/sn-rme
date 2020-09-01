418795a430e5ae2e85feffa3955d2af4c23ed583
Arturo Linares <arturo.linares@phase2technology.com>
2020-09-01 09:53:28 -0500

    Converted the editor to a functional component.

--- src/lib/components/LocalEditor.js
+++ src/lib/components/LocalEditor.js
@@ -1,97 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { debounce } from "lodash";
-import RichMarkdownEditor, { theme } from "rich-markdown-editor";
+import RichMarkdownEditor from "rich-markdown-editor";
 import BridgeManager from "../BridgeManager";
 import light from '../theme';
 
-
-class YoutubeEmbed extends React.Component {
-  render() {
-    const { attrs } = this.props;
-    const videoId = attrs.matches[1];
-
-    return (
-      <iframe
-        src={`https://www.youtube.com/embed/${videoId}?modestbranding=1`}
-      />
-    );
-  }
+const YoutubeEmbed = function ({ attrs = {} }) {
+  const videoId = attrs.matches[1];
+  return <iframe
+    src={`https://www.youtube.com/embed/${videoId}?modestbranding=1`}
+  />;
 }
 
-export default class LocalEditor extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {};
-  }
+export default function LocalEditor() {
+  const [note, setNote] = useState({});
+  const [markdown, setMarkdown] = useState("");
 
-  componentDidMount() {
-    BridgeManager.get().addUpdateObserver(() => {
-      const note = BridgeManager.get().getNote();
-      const refresh = !this.state.note
-        || (this.state.note && this.state.note.uuid != note.uuid);
-      this.setState({
-        note: BridgeManager.get().getNote(),
-      });
-      if (refresh) {
-        this.updateMarkdown();
-      }
-    });
-  }
-
-  updateMarkdown() {
-    let markdown = this.state.note.content.text
-      .replace(/(\n{2})(\n+)/g, (m, p, q) => p + q.replace(/(\n)/g, '\\$1'));
-    if (markdown == "") {
-      markdown = "\n";
-    }
-    this.setState({ markdown });
-  }
-
-  onChange = debounce((value) => {
+  const  onChange = debounce((value) => {
     const text = value();
-    let note = this.state.note;
-    note.content.text = text;
-    this.setState({ note: note });
+    const newNote = JSON.parse(JSON.stringify(note));
+    newNote.content = text;
+    setNote(newNote);
     BridgeManager.get().save();
-  })
+  }, 666);
 
-  getNoteContents() {
-    if (this.state.note) {
-      return this.state.note.content.text;
+  const updateMarkdown = (newNote) => {
+    let newMarkdown = newNote.content.text
+      .replace(/(\n{2})(\n+)/g, (m, p, q) => p + q.replace(/(\n)/g, '\\$1'));
+
+    if (newMarkdown == "") {
+      newMarkdown = "\n";
     }
-    return '';
-  }
 
-  render() {
-    return (
-      <div>
-        <RichMarkdownEditor
-          value={ this.state.markdown }
-          placeholder=""
-          autoFocus
-          onChange={ this.onChange.bind(this) }
-          theme={light}
-          embeds={[
-            {
-              title: "YouTube",
-              keywords: "youtube video tube google",
-              icon: () => (
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/7/75/YouTube_social_white_squircle_%282017%29.svg"
-                  width={24}
-                  height={24}
-                />
-              ),
-              matcher: url => {
-                return url.match(
-                  /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([a-zA-Z0-9_-]{11})$/i
-                );
-              },
-              component: YoutubeEmbed,
+    setMarkdown(newMarkdown);
+  };
+
+  BridgeManager.get().addUpdateObserver(() => {
+    const newNote = BridgeManager.get().getNote();
+    const refresh = !note
+      || (note && note.uuid != newNote.uuid);
+    setNote(newNote);
+
+    if (refresh) {
+      updateMarkdown(newNote);
+    }
+  });
+
+  return (
+    <>
+      <RichMarkdownEditor
+        value={ markdown }
+        placeholder=""
+        autoFocus
+        onChange={ onChange }
+        theme={light}
+        embeds={[
+          {
+            title: "YouTube",
+            keywords: "youtube video tube google",
+            icon: () => (
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/7/75/YouTube_social_white_squircle_%282017%29.svg"
+                width={24}
+                height={24}
+              />
+            ),
+            matcher: url => {
+              return url.match(
+                /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([a-zA-Z0-9_-]{11})$/i
+              );
             },
-          ]}
-        />
-      </div>
-    );
-  }
+            component: YoutubeEmbed,
+          },
+        ]}
+      />
+    </>
+  );
 }
