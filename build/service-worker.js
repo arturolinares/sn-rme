68e04b7d739025087464211ba0632b1c5b3a3408
Arturo Linares <arturo.linares@phase2technology.com>
2020-11-25 13:24:36 -0600

    Fixes #20.
    After updating the editor to the latest version the list items were not wrapping correctly, probably caused by a third party library change.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.00f13e22fff7bbd5cb05788e322b4e59.js"
+  "./precache-manifest.112aaa7635c3e6647e07177b7e8d4075.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
