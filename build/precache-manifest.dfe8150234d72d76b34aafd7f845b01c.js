9fecd2e1a994ad97a77371362cce0fd6ff98981c
Arturo Linares <arturo.linares@gmail.com>
2021-02-20 23:25:42 -0600

    Updated the editor from upstream. Fixes #21 and #14.

---
+++ build/precache-manifest.dfe8150234d72d76b34aafd7f845b01c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acecf4ecb5de3869ba51115b5add4e3f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "806f4c576188871e88c6",
+    "url": "./static/css/2.16816ef8.chunk.css"
+  },
+  {
+    "revision": "d65248511a5fcf3f29f9",
+    "url": "./static/css/main.b8153379.chunk.css"
+  },
+  {
+    "revision": "806f4c576188871e88c6",
+    "url": "./static/js/2.85492898.chunk.js"
+  },
+  {
+    "revision": "54335a5caf6bd5c45ec571cb34ffcd63",
+    "url": "./static/js/2.85492898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65248511a5fcf3f29f9",
+    "url": "./static/js/main.0caa7040.chunk.js"
+  },
+  {
+    "revision": "3fae0b9d79f6c70627b6",
+    "url": "./static/js/runtime-main.5a852d11.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.112aaa7635c3e6647e07177b7e8d4075.js"
+  "./precache-manifest.dfe8150234d72d76b34aafd7f845b01c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
