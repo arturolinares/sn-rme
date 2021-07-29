58f165208b0865ce62f994dff1a37e88e217078a
Bruno Bernardino <me@brunobernardino.com>
2021-07-29 15:08:38 +0100

    Build.

---
+++ docs/precache-manifest.3e3a27a860fc9a48d50f72cb09a483dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec65008689ffbfdb4cdf18f54c71fa35",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73b92942fd529f3cad7a",
+    "url": "./static/css/2.c7d0933c.chunk.css"
+  },
+  {
+    "revision": "7c34d5b7a1fee441d88a",
+    "url": "./static/css/main.3d009a7c.chunk.css"
+  },
+  {
+    "revision": "73b92942fd529f3cad7a",
+    "url": "./static/js/2.1bc7ca00.chunk.js"
+  },
+  {
+    "revision": "1a59fb0384be61751a9d1d85a311784a",
+    "url": "./static/js/2.1bc7ca00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c34d5b7a1fee441d88a",
+    "url": "./static/js/main.a91efa07.chunk.js"
+  },
+  {
+    "revision": "3fae0b9d79f6c70627b6",
+    "url": "./static/js/runtime-main.5a852d11.js"
+  }
+]);
